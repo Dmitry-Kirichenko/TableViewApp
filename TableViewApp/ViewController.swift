7a55fdae84db2212cacd74c9e8f57f50e0485238
Dmitry Kyrychenko <kirichenkodmitryy@gmail.com>
2020-10-24 11:58:24 +0300

    Initial Commit

---
+++ TableViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by Дмитрий Кириченко on 24.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
